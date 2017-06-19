5f12e2a69d574f9a25b4f838802fc8c221ff02f1
Hamza Saleem <hamzasaleemzpr@gmail.com>
2017-06-19 11:42:37 +0500

    Fix Issue Set Private (off) appearing twice in menu

--- QKSMS/src/main/java/com/moez/QKSMS/ui/messagelist/MessageListFragment.java
+++ QKSMS/src/main/java/com/moez/QKSMS/ui/messagelist/MessageListFragment.java
@@ -475,7 +475,6 @@ public class MessageListFragment extends QKFragment implements ActivityLauncher,
                 makeCall();
                 return true;
 
-            case R.id.menu_uw_convo_private_text:
             case R.id.menu_uw_convo_private:
                 boolean conversationPrivate = !mConversationPrefs.getUWConversationPrivateEnabled();
                 mConversationPrefs.putBoolean(SettingsFragment.UW_CONVO_PRIVATE, conversationPrivate);
@@ -677,9 +676,6 @@ public class MessageListFragment extends QKFragment implements ActivityLauncher,
                  R.string.menu_uw_convo_private_off : R.string.menu_uw_convo_private);
         menu.findItem(R.id.menu_uw_convo_private).setIcon(mConversationPrefs.getUWConversationPrivateEnabled() ?
                 R.drawable.ic_visibility_off : R.drawable.ic_visibility);
-
-        menu.findItem(R.id.menu_uw_convo_private_text).setTitle(mConversationPrefs.getUWConversationPrivateEnabled() ?
-                R.string.menu_uw_convo_private_off : R.string.menu_uw_convo_private);
 
         menu.findItem(R.id.menu_notifications).setTitle(mConversationPrefs.getNotificationsEnabled() ?
                 R.string.menu_notifications : R.string.menu_notifications_off);
